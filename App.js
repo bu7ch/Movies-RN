1e08a53dcada0e24fb10a8e6f0fc86fb16278a01
Rodolphe <dutchr@live.fr>
2022-01-04 11:03:36 +0100

    display api data for one film

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import axios from 'axios'
-
-
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
 
 const getPopularMovies = async () => {
-  const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4e2a93812004fddd622fb5e91194b8a9')
-  console.log(JSON.stringify(res.data.results[0], null, 2));
-}
+  const res = await axios.get(
+    "https://api.themoviedb.org/3/movie/popular?api_key=4e2a93812004fddd622fb5e91194b8a9"
+  );
+  return res.data.results
+};
 const App = () => {
- getPopularMovies()
+  const [movie, setMovie] = useState("");
+  getPopularMovies().then((movies) => {
+    setMovie(movies[0]);
+  });
   return (
     <View>
-      <Text></Text>
+      <Text> Nom du film : {movie.original_title}</Text>
+      <Text> Langue : {movie.original_language}</Text>
+      <Text> Date de sortie : {movie.release_date}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
