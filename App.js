bc89e37bf9766dc97f350dea120c048971f24c4d
Rodolphe <dutchr@live.fr>
2022-01-04 10:14:35 +0100

    test api

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text } from 'react-native'
+import axios from 'axios'
 
-export default function App() {
+
+
+const getPopularMovies = async () => {
+  const res = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=4e2a93812004fddd622fb5e91194b8a9')
+  console.log(JSON.stringify(res.data.results[0], null, 2));
+}
+const App = () => {
+ getPopularMovies()
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text></Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
