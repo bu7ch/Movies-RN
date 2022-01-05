2c0bf6f53c369ed1757eac5caa17310f6e1de73f
Rodolphe <dutchr@live.fr>
2022-01-05 12:37:32 +0100

    :sparkles: Display image on detail screen

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import {  Text } from 'react-native'
+import React, { useState, useEffect } from "react";
+import { Image, ScrollView, Text, StyleSheet } from "react-native";
+import { getMovie } from "../services/services";
+const placeholder = require("../assets/placeholder.png");
+const Detail = ({ route, navigation }) => {
+  const [movieDetail, setmovieDetail] = useState();
+  const [loaded, setLoaded] = useState(false);
+  const movieId = route.params.movieDetail.id;
 
-const Detail = () => {
+  useEffect(() => {
+    getMovie(movieId).then((movieData) => {
+      setmovieDetail(movieData);
+      setLoaded(true);
+    });
+  }, [movieId]);
   return (
     <>
-      <Text>{"Movie Detail"}</Text>
+      {loaded && (
+        <ScrollView>
+          <Image
+            resizeMode="cover"
+            style={styles.image}
+            source={
+              movieDetail.poster_path
+                ? {
+                    uri:
+                      "https://image.tmdb.org/t/p/w500" +
+                      movieDetail.poster_path,
+                  }
+                : placeholder
+            }
+          />
+        </ScrollView>
+      )}
     </>
-  )
-}
+  );
+};
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 125,
+    borderRadius: 20,
+  },
+});
 
-export default Detail
+export default Detail;
--- services/services.js
+++ services/services.js
@@ -1,32 +1,25 @@
 import axios from "axios";
 
 const api_url = "https://api.themoviedb.org/3";
-const api_key = "api_key=4e2a93812004fddd622fb5e91194b8a9"
+const api_key = "api_key=4e2a93812004fddd622fb5e91194b8a9";
 
 export const getPopularMovies = async () => {
-  const result = await axios.get(
-    `${api_url}/movie/popular?${api_key}`
-  );
+  const result = await axios.get(`${api_url}/movie/popular?${api_key}`);
   return result.data.results;
 };
 
-
 export const getUpcomingMovies = async () => {
-  const result = await axios.get(
-    `${api_url}/movie/upcoming?${api_key}`
-  );
+  const result = await axios.get(`${api_url}/movie/upcoming?${api_key}`);
   return result.data.results;
 };
 export const getPopularTv = async () => {
-  const result = await axios.get(
-    `${api_url}/tv/popular?${api_key}`
-  );
+  const result = await axios.get(`${api_url}/tv/popular?${api_key}`);
   return result.data.results;
 };
 // Get Family Movies
 export const getFamilyMovies = async () => {
   const resp = await axios.get(
-    `${api_url}/discover/movie?${api_key}&with_genres=10751`,
+    `${api_url}/discover/movie?${api_key}&with_genres=10751`
   );
   return resp.data.results;
 };
@@ -34,7 +27,14 @@ export const getFamilyMovies = async () => {
 // Get Documnetery Movies
 export const getDocumentaryMovies = async () => {
   const resp = await axios.get(
-    `${api_url}/discover/movie?${api_key}&with_genres=99`,
+    `${api_url}/discover/movie?${api_key}&with_genres=99`
   );
   return resp.data.results;
+};
+
+// Get Movie by ID
+
+export const getMovie = async (id) => {
+  const res = await axios.get(`${api_url}/movie/${id}?${api_key}`);
+  return res.data;
 };
