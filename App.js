0a88fbf152ce517ce858a4c43b34f5f5c2ba0a20
Rodolphe <dutchr@live.fr>
2022-01-05 11:44:19 +0100

    :sparkles: Adding react Navigation & Detail screen

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { View, StyleSheet } from "react-native";
+import React from "react";
 import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Detail from "./screens/Detail";
 
+const Stack = createNativeStackNavigator();
 
 const App = () => (
- 
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Detail" component={Detail} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
-    <View style={styles.container}>
-      <Home></Home>
-    </View>
-  );
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
 export default App;
