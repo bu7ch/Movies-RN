2017d5dcdd91f2e09c0c929d2083aacb1c02e3c9
Rodolphe <dutchr@live.fr>
2022-01-05 17:06:17 +0100

    :sparkles: Display all datas in screen detail from the API

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Image, ScrollView, Text, StyleSheet } from "react-native";
+import { Image, ScrollView, Text, StyleSheet, View, Dimensions } from "react-native";
 import { getMovie } from "../services/services";
+import StarRating from "react-native-star-rating"; 
+import dateFormat from 'dateformat'
 const placeholder = require("../assets/placeholder.png");
+
+const height = Dimensions.get("screen").height;
 const Detail = ({ route, navigation }) => {
   const [movieDetail, setmovieDetail] = useState();
   const [loaded, setLoaded] = useState(false);
@@ -30,6 +34,26 @@ const Detail = ({ route, navigation }) => {
                 : placeholder
             }
           />
+          <View style={styles.container}>
+            <Text style={styles.movieTitle}>{movieDetail.title}</Text>
+            {movieDetail.genres && ( 
+              <View style={styles.genresContainer}>
+                {movieDetail.genres.map((genre) => {
+                  return (
+                    <Text style={styles.genre} key={genre.id}>{genre.name}</Text>
+                  )
+                })}
+                </View>
+            )}
+            <StarRating 
+            disabled={true}
+            maxStars={5}
+            starSize={30}
+            rating={movieDetail.vote_average /2}
+            fullStarColor={"gold"}/>
+            <Text style={styles.overview}> {movieDetail.overview}</Text>
+            <Text style={styles.release}> {"Date de sortie : " + dateFormat(movieDetail.release_date, "dS mmmm, yyyy")}</Text>
+          </View>
         </ScrollView>
       )}
     </>
@@ -37,10 +61,34 @@ const Detail = ({ route, navigation }) => {
 };
 const styles = StyleSheet.create({
   image: {
-    height: 200,
-    width: 125,
-    borderRadius: 20,
+    height: height / 2.5,
   },
+  container: { 
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  movieTitle: {
+    fontSize: 24,
+    fontWeight:'bold',
+    marginTop:10,
+    marginBottom:10
+  },
+  genresContainer:{
+    flexDirection:"row",
+    alignContent: "center",
+    marginTop:20,
+    marginBottom:20
+  },
+  genre: {
+    marginRight:10,
+    fontWeight:'bold'
+  }, overview: {
+    padding:15,
+  },
+  release:{
+    fontWeight: "bold"
+  }
 });
 
 export default Detail;
--- services/services.js
+++ services/services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api_url = "https://api.themoviedb.org/3";
-const api_key = "api_key=4e2a93812004fddd622fb5e91194b8a9";
+const api_key = "api_key=4e2a93812004fddd622fb5e91194b8a9&language=fr";
 
 export const getPopularMovies = async () => {
   const result = await axios.get(`${api_url}/movie/popular?${api_key}`);
